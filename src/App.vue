88a279df49d7f1490a15841fb5bc05713f0f6968
AASosnin <sosnin.sa@yandex.ru>
2020-11-19 10:58:51 +0400

    Filter, add ready

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FormExpenses @onFormSubmit="addNewItem"></FormExpenses>
+    <BudgetTotal />
+    <Expenses></Expenses>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapGetters } from 'vuex';
+import { mapActions } from 'vuex';
+import Expenses from "./components/Exepenses";
+import BudgetTotal from "./components/BudgetTotal";
+import FormExpenses from "./components/FormExpenses";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {FormExpenses, BudgetTotal, Expenses},
+  computed: {
+    ...mapGetters('expensesStore', ['expensesAll']),
+  },
+  methods: {
+    ...mapActions('expensesStore', ['addNewItem']),
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #808080;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
